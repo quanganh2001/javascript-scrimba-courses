564ac38270da5b5d3be62fc40409569ea880dfd2
quanganh2001 <cocoi5659@gmail.com>
2023-01-19 22:21:39 +0700

    Concatenate the two variables JS

---
+++ practical-part1/practices/variables.js
@@ -0,0 +1,7 @@
+// Create two variables, firstName and lastName
+let firstName = "Quang Anh";
+let lastName = "Nguyen";
+// Concatenate the two variables into a third variable called fullName
+let fullName = lastName + " " + firstName;
+// Log fullName to the console
+console.log(fullName);
